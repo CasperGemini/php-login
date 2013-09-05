da3214fb837dd5b1e2f99bd27615c8d001c52824
CasperGemini <CasperGemini@Gmail.com>
2013-09-04 20:06:17 -0500

    Update config.php
    Update Line 13: Spelling correction (differen) to (different)

--- 4-full-mvc-framework/config/config.php
+++ 4-full-mvc-framework/config/config.php
@@ -10,7 +10,7 @@
 /**
  * Configuration for: Base URL
  * This is the base url of our app. if you go live with your app, put your full domain name here.
- * if you are using a (differen) port, then put this in here, like http://mydomain:8888/mvc/
+ * if you are using a (different) port, then put this in here, like http://mydomain:8888/mvc/
  * TODO: when not using subfolder, is the trailing slash important ?
  */
 define('URL', 'http://127.0.0.1/php-login/4-full-mvc-framework/');
